f1692ffa34a8f95a36b13205dfd137a804a8fa2e
James Brown <brownj95@gmail.com>
2021-04-07 18:19:50 +0000

    added ctrl+C to exit

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
 const  { connect } = require('./client');
 
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.on('data', handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+
 console.log('Connecting ...');
 
 connect();
+
+setupInput();
