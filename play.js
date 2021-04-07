49984987cb208f2775f89fc2e671610c3bde829e
James Brown <brownj95@gmail.com>
2021-04-07 17:22:03 +0000

    set up client side

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => console.log('you ded cuz u idled'));
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
