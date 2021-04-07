5a3e83389a938398369de85cb318f35901976884
James Brown <brownj95@gmail.com>
2021-04-07 18:36:36 +0000

    add full movement key handling

--- input.js
+++ input.js
@@ -1,20 +1,33 @@
-const handleUserInput = (key) => { //=> if user input is ctrl+C, exit the program
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+let connection;
 
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.on('data', handleUserInput);
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') { //=> if user input is ctrl+C, exit the program
+    process.exit();
+  }
+
+  if (key === 'w') { //=> handle directional keys
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
+  }
 };
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,8 +1,6 @@
 const  { connect } = require('./client');
 const { setupInput } = require('./input');
 
-
 console.log('Connecting ...');
-connect();
 
-setupInput();
+setupInput(connect());
