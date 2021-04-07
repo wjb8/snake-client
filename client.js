15f92db5b22fa7cbd5e17b3092ecad2910d6ebda
James Brown <brownj95@gmail.com>
2021-04-07 17:35:48 +0000

    added write name to conn obj

--- client.js
+++ client.js
@@ -2,13 +2,18 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
- const connect = function() {
+const connect = function() {
   const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('Connection established!');
+    conn.write('Name: JB');
+  });
 
   conn.on('data', (data) => console.log('you ded cuz u idled'));
 
